3190d2a630651a1bc0adac1d0e7e1cd6b67fd1f3
Isaac Rodriguez <isaacs734@gmail.com>
2026-05-03 04:34:06 -0600

    agregar debug

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ API_KEY = os.getenv("a62dd4ce99msha865405af45e6ffp1ccb6bjsnee9703603f17")
 def home():
     return {"status": "ok"}
 
-@app.get("/picks")
-def picks():
-    url = url = "https://api-football-v1.p.rapidapi.com/v3/fixtures?date=2026-05-03"
+@app.get("/debug")
+def debug():
+    url = "https://api-football-v1.p.rapidapi.com/v3/fixtures?date=2026-05-03"
 
     headers = {
         "X-RapidAPI-Key": API_KEY,
@@ -20,14 +20,4 @@ def picks():
     }
 
     response = requests.get(url, headers=headers)
-    data = response.json()
-
-    partidos = []
-
-    for match in data.get("response", [])[:5]:
-        partidos.append({
-            "match": match["teams"]["home"]["name"] + " vs " + match["teams"]["away"]["name"],
-            "minuto": match["fixture"]["status"]["elapsed"]
-        })
-
-    return partidos
+    return response.json()
